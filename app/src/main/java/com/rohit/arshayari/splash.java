e2f58b33b8684c8a0d31da476f231d4c35b9ac3b
rohit8532 <rohityadav853202@gmail.com>
2023-10-05 02:23:31 +0530

    first commit

---
+++ app/src/main/java/com/rohit/arshayari/splash.java
@@ -0,0 +1,21 @@
+package com.rohit.arshayari;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(() -> {
+            Intent i=new Intent(splash.this,MainActivity.class);
+            startActivity(i);
+            finish();
+        },2000);
+    }
+}
