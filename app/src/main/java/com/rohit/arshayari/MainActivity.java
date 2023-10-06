9372ee09d57dc30c4026171103b8007752a49276
rohit8532 <rohityadav853202@gmail.com>
2023-10-07 02:15:22 +0530

    first commit

--- app/src/main/java/com/rohit/arshayari/MainActivity.java
+++ app/src/main/java/com/rohit/arshayari/MainActivity.java
@@ -2,13 +2,26 @@ package com.rohit.arshayari;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn=findViewById(R.id.btn1);
+        btn.setOnClickListener(view -> {
+            Intent i = new Intent(MainActivity.this,second.class);
+            startActivity(i);
+        });
+        btn=findViewById(R.id.btn2);
+        btn.setOnClickListener(view -> finishAffinity());
+
     }
 }
